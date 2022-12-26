7fe14663c84879f6ba94b5369b5c4000a61d0f52
user <user@DESKTOP-8D7LR2I>
2022-12-26 12:38:53 +0800

    12261238

--- BradJava/src/tw/com/ispan/myjava/Brad01.java
+++ BradJava/src/tw/com/ispan/myjava/Brad01.java
@@ -2,6 +2,6 @@ package tw.com.ispan.myjava;
 
 public class Brad01 {
 	public static void main(String[] args) {
-		System.out.println("Hello, WorldV2");
+		System.out.println("Hello, WorldV1");
 	}
 }
