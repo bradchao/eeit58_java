7a714e98a330a87f916e0bb2e75321eb12f14e77
user <user@DESKTOP-8D7LR2I>
2022-12-26 12:37:14 +0800

    iii

---
+++ BradJava/src/tw/com/ispan/myjava/Brad02.java
@@ -0,0 +1,11 @@
+package tw.com.ispan.myjava;
+
+public class Brad02 {
+	public static void main(String[] args) {
+		// 變數 => 型別 => 基本 / 物件
+		// 整數 / 浮點數 / 字元 / 布林 => 8
+		byte var1;
+		byte $_$ = 123;
+		System.out.println($_$);
+	}
+}
---
+++ BradJava/src/tw/com/ispan/myjava/HelloWorld.java
@@ -0,0 +1,7 @@
+package tw.com.ispan.myjava;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, Brad");
+	}
+}
