78aca5be2b345ddfda73cc1e2d2de6be1b8f4541
user <user@DESKTOP-8D7LR2I>
2023-01-09 16:30:39 +0800

    01091630

--- BradJava/src/tw/com/ispan/myjava/Brad56.java
+++ BradJava/src/tw/com/ispan/myjava/Brad56.java
@@ -1,14 +1,35 @@
 package tw.com.ispan.myjava;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 public class Brad56 {
 
 	public static void main(String[] args) {
-
+		Student s1 = new Student("Brad", 80, 32, 13);
+		System.out.println(s1);
+		System.out.println(s1.sum());
+		System.out.println(s1.avg());
+		
+		try {
+			ObjectOutputStream oout = new ObjectOutputStream(
+				new FileOutputStream("dir1/brad.score"));
+			oout.writeObject(s1);
+			oout.flush();
+			oout.close();
+			System.out.println("OK");
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
 	}
 
 }
 
-class Student {
+class Student implements Serializable {
 	private int ch, eng, math;
 	private String name;
 	Student(String name, int ch, int eng, int math){
