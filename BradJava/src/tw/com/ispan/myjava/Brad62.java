02bc858cf9352ff1bc6df3d2bd9d61826cbde2d8
user <user@DESKTOP-8D7LR2I>
2023-01-31 11:15:08 +0800

    01311115

--- BradJava/src/tw/com/ispan/myjava/Brad62.java
+++ BradJava/src/tw/com/ispan/myjava/Brad62.java
@@ -8,12 +8,12 @@ import java.net.SocketException;
 public class Brad62 {
 
 	public static void main(String[] args) {
-		String mesg = "Hello, World";
+		String mesg = "Hello, World from Brad 3";
 		byte[] data = mesg.getBytes();
 		try {
 			DatagramSocket socket = new DatagramSocket();
 			DatagramPacket packet = new DatagramPacket(data, data.length, 
-					InetAddress.getByName("10.10.243.1"), 8888);
+					InetAddress.getByName("10.10.243.255"), 8888);
 			socket.send(packet);
 			socket.close();
 			System.out.println("OK");
--- BradJava/src/tw/com/ispan/myjava/Brad63.java
+++ BradJava/src/tw/com/ispan/myjava/Brad63.java
@@ -7,16 +7,28 @@ import java.net.SocketException;
 public class Brad63 {
 
 	public static void main(String[] args) {
-		byte[] buf = new byte[1024];
-		try {
-			DatagramSocket socket = new DatagramSocket(8888);
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			System.out.println("waiting...");
-			socket.receive(packet);
-			socket.close();
-			System.out.println("Receive OK");
-		} catch (Exception e) {
-			System.out.println(e);
+		while (true) {
+			byte[] buf = new byte[1024];
+			try {
+				DatagramSocket socket = new DatagramSocket(8888);
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				System.out.println("waiting...");
+				socket.receive(packet);
+				socket.close();
+				
+				String urip = packet.getAddress().getHostAddress();
+				int len = packet.getLength();
+				byte[] data = packet.getData();
+				String mesg = new String(data, 0, len);
+				System.out.printf("%s => %s\n", urip, mesg);
+				
+				if (mesg.equals("quit")) {
+					break;
+				}
+				
+			} catch (Exception e) {
+				System.out.println(e);
+			}
 		}
 		
 	}
