09bb9fd4a34299f00717907bb8d02b152d9f14f2
user <user@DESKTOP-8D7LR2I>
2023-02-01 16:31:16 +0800

    02011631

---
+++ BradJava/src/tw/com/ispan/myjava/Brad72.java
@@ -0,0 +1,11 @@
+package tw.com.ispan.myjava;
+
+public class Brad72 {
+
+	public static void main(String[] args) {
+		double v1 = 12.5;
+		System.out.println(Math.ceil(v1));
+		System.out.println(Math.floor(v1));
+	}
+
+}
