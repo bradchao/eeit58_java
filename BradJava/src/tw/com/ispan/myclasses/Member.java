104c3e10b464992a039a3a4f1030503f96c186a6
user <user@DESKTOP-8D7LR2I>
2023-02-02 11:14:07 +0800

    02021114

--- BradJava/src/tw/com/ispan/myclasses/Member.java
+++ BradJava/src/tw/com/ispan/myclasses/Member.java
@@ -1,5 +1,34 @@
 package tw.com.ispan.myclasses;
 
 public class Member {
-
+	private int id;
+	private String account, realname, birthday;
+	public Member(int id, String account, String realname, String birthday) {
+		this.id = id; this.account = account;
+		this.realname = realname; this.birthday = birthday;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	
+	
 }
--- BradJava/src/tw/com/ispan/myjava/JDBC08.java
+++ BradJava/src/tw/com/ispan/myjava/JDBC08.java
@@ -8,12 +8,14 @@ import java.util.Properties;
 import java.util.Scanner;
 
 import tw.com.ispan.myclasses.BCrypt;
+import tw.com.ispan.myclasses.Member;
 
 public class JDBC08 {
 	private final static String USER = "root"; 
 	private final static String PASSWORD = "root";
 	private final static String URL = "jdbc:mysql://localhost:3306/iii";
 	private final static String SQL_LOGIN = "SELECT * FROM member WHERE account = ?";
+	private final static String SQL_CHPASSWD = "UPDATE member SET passwd = ? WHERE id = ?";
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
@@ -34,15 +36,39 @@ public class JDBC08 {
 			if (rs.next()) {
 				if (BCrypt.checkpw(passwd, rs.getString("passwd"))) {
 					// login
+					Member member = new Member(rs.getInt("id"),
+							rs.getString("account"), rs.getString("realname"), null);
+					System.out.printf(
+						"Welcome, %s / %s\n", member.getRealname(), member.getAccount());
 					
+					// change password process
+					System.out.print("Change Password(newpasswd or NO): ");
+					String newPasswd = scanner.next();
 					
+					if (newPasswd.length() >= 6) {
+						PreparedStatement pstmt2 = conn.prepareStatement(SQL_CHPASSWD);
+						pstmt2.setString(1, BCrypt.hashpw(newPasswd, BCrypt.gensalt()));
+						pstmt2.setInt(2, member.getId());
+						if (pstmt2.executeUpdate() > 0 ) {
+							// SUCCESS
+							System.out.println("change passwd success!");
+						}else {
+							// 更換失敗
+							System.out.println("change passwd failure!");
+						}
+					}else {
+						// 不想換
+						System.out.println("no change");
+					}
 					
 					
 				}else {
 					// failure
+					System.out.println("login failure(1)");
 				}
 			}else {
 				// 帳號不存在
+				System.out.println("login failure(2)");
 			}
 			
 		}catch (Exception e) {
